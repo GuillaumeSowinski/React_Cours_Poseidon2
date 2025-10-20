088d5e25e3c61b092cb5db886227358802727327
Guillaume <guillaume@sowinski.fr>
2025-10-20 17:06:29 +0200

    hotfix

--- src/App.jsx
+++ src/App.jsx
@@ -41,8 +41,8 @@ function App() {
   }
 
 
-  const modifyFullProduct = async () => {
-    const response = await fetch('https://fakestoreapi.com/products/1', {
+  const modifyFullProduct = async (id) => {
+    const response = await fetch(`https://fakestoreapi.com/products/${id}`, {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
@@ -59,8 +59,8 @@ function App() {
     alert(`Le produit avec l'id ${data.id} a été modifié`)
   }
 
-  const modifyProduct = async () => {
-    const response = await fetch('https://fakestoreapi.com/products/1', {
+  const modifyProduct = async (id) => {
+    const response = await fetch(`https://fakestoreapi.com/products/${id}`, {
       method: "PATCH",
       headers: {
         "Content-Type": "application/json",
@@ -73,8 +73,8 @@ function App() {
     alert(`Le prix du produit avec l'id ${data.id} a été modifié`)
   }
 
-  const deleteProduct = async () => {
-    const response = await fetch('https://fakestoreapi.com/products/1', {
+  const deleteProduct = async (id) => {
+    const response = await fetch(`https://fakestoreapi.com/products/${id}`, {
       method: "DELETE",
     })
     const data = await response.json()
@@ -100,9 +100,9 @@ function App() {
 
               </Card.Body>
               <Card.Footer className='row gy-3 mx-0'>
-                <Button variant="primary" onClick={modifyFullProduct}>Modifier le produit complet</Button>
-                <Button variant="warning" onClick={modifyProduct}>Modifier le prix du produit</Button>
-                <Button variant="danger" onClick={deleteProduct}>Supprimer le produit</Button>
+                <Button variant="primary" onClick={() => modifyFullProduct(item.id)}>Modifier le produit complet</Button>
+                <Button variant="warning" onClick={() => modifyProduct(item.id)}>Modifier le prix du produit</Button>
+                <Button variant="danger" onClick={() => deleteProduct(item.id)}>Supprimer le produit</Button>
               </Card.Footer>
             </Card>
           </Col>
