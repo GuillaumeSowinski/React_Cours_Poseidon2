44d0c29442aff7985459436414b989a7f3560792
Guillaume <guillaume@sowinski.fr>
2025-10-20 16:44:08 +0200

    exercices 5 à 7

--- src/App.jsx
+++ src/App.jsx
@@ -40,6 +40,47 @@ function App() {
     alert(`Le produit avec l'id ${data.id} a été créé`)
   }
 
+
+  const modifyFullProduct = async () => {
+    const response = await fetch('https://fakestoreapi.com/products/1', {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: "Produit mis à jour",
+        price: 49.99,
+        description: "Description mise à jour",
+        image: "https://picsum.photos/200/300",
+        category: "electronics",
+      }),
+    })
+    const data = await response.json()
+    alert(`Le produit avec l'id ${data.id} a été modifié`)
+  }
+
+  const modifyProduct = async () => {
+    const response = await fetch('https://fakestoreapi.com/products/1', {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        price: 39.99,
+      }),
+    })
+    const data = await response.json()
+    alert(`Le prix du produit avec l'id ${data.id} a été modifié`)
+  }
+
+  const deleteProduct = async () => {
+    const response = await fetch('https://fakestoreapi.com/products/1', {
+      method: "DELETE",
+    })
+    const data = await response.json()
+    alert(`Le produit avec l'id ${data.id} a été supprimé`)
+  }
+
   return (
     <Container className='mt-5'>
       <Button onClick={addProduct}>Ajouter un  Produit</Button>
@@ -56,7 +97,13 @@ function App() {
                 <Card.Title>{item.title}</Card.Title>
                 <Card.Text>{item.description}</Card.Text>
                 <Card.Text>{item.price}€</Card.Text>
+
               </Card.Body>
+              <Card.Footer className='row gy-3 mx-0'>
+                <Button variant="primary" onClick={modifyFullProduct}>Modifier le produit complet</Button>
+                <Button variant="warning" onClick={modifyProduct}>Modifier le prix du produit</Button>
+                <Button variant="danger" onClick={deleteProduct}>Supprimer le produit</Button>
+              </Card.Footer>
             </Card>
           </Col>
         ))}
